3c2500e3bebe1a2ea382e455b0b6f321030245a1
Morgan Martin <63368697+MorganMart1n@users.noreply.github.com>
2024-02-25 07:53:48 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php include 'connect.php';
+
+
+
+$query = $_POST['username'];
+
+$raw_results = mysqli_query($conn, "SELECT * FROM user
+    WHERE username LIKE '%".$query."%' OR
+    'text' LIKE '%".$query."%'") or die(mysqli_error($conn));
+
+if(mysqli_num_rows($raw_results) > 0){
+    while($results = mysqli_fetch_array($raw_results)){
+        header("Location: dashboard.php");
+    }}else {
+    echo "Login failed";
+}
+
+mysqli_close($conn);
+?>
+
+
+
+
+
+
+
+
+
+
