030ddcb8167de0abf1c3369cf5e98bb6ab18f56a
stuartiek <stuartiek@gmail.com>
2024-02-25 07:58:44 +0000

    changes to dashboard

--- dashboard.php
+++ dashboard.php
@@ -9,21 +9,50 @@
         <link rel="stylesheet" href="public/css/main.css"/>
     </head>
 <body>
-    <header>
-        <div class="header">
-            <div>
-                <img src="public/images/core29-logo.png" alt="">
+        <header>
+            <div class="header">
+                <div>
+                    <img src="public/images/core29-logo.png" alt="">
+                </div>
+                <div>
+                    <nav>
+                        <ul>
+                            <li><a href = "dashboard.php">Home</a></li>
+                            <li><a href = "commuting.php">Commuting Insights</a></li>
+                            <li><a href = "energyUse.php">Energy tracker</a></li>
+                            <li><a href = "co2.php">Emissions tracker</a></li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-            <div>
-                <nav>
-                    <ul>
-                        <li><a href = "dashboard.php">Home</a></li>
-                        <li><a href = "commuting.php">Commuting Insights</a></li>
-                        <li><a href = "energyUse.php">Energy tracker</a></li>
-                        <li><a href = "co2.php">Emissions tracker</a></li>
-                    </ul>
-                </nav>
-            </div>
+        </header>
+    <div class="dashInfo">
+        <div class="dashInfo1">
+            <h2>Can We Help</h2>
+            <p>+44 (0)1224 937729</p>
+            <button>Send Email</button>
         </div>
+        <div class="dashInfo2">
+            <h2>Tracking Your Carbon Footprint</h2>
+            <p>Core29 is a cutting-edge web application 
+               designed to empower individuals and businesses 
+               to monitor and reduce their carbon footprint. 
+               This user-friendly platform utilizes advanced tracking 
+               algorithms and real-time data inputs to provide comprehensive 
+               insights into carbon emissions across various aspects of daily life and operations.</p>
+        </div>
+    </div>
+
+
+
+
+
+
+    <footer>
+        <div class="footer">
+            <p>Core29 &copy; 2024 | All Rights Reserved</p>
+            <img src="public/images/core29-logo-white.png" alt="">
+        </div>
+    </footer>
 </body>
 </html>
