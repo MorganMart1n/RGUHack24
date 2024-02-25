d0e5e7c34ccdb6c06bec7208f4ecc92904272b16
Morgan Martin <63368697+MorganMart1n@users.noreply.github.com>
2024-02-25 02:46:45 +0000

    Added base for task 2

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "co2";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+
+
+exit;
+?>
