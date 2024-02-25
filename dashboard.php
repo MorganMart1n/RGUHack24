1dd0339130f27466631e492b1ffbd67f7ca0e8c6
Morgan Martin <63368697+MorganMart1n@users.noreply.github.com>
2024-02-25 03:56:55 +0000

    Updated and re did all of the files

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<?php include 'connect.php'?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Core29</title>
+        <link rel="icon" type="image" href="public/images/favicon-128x128.png">
+        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+        <link rel="stylesheet" href="public/css/main.css"/>
+    </head>
+<body>
+    <header>
+        <div class="header">
+            <div>
+                <img src="public/images/core29-logo.png" alt="">
+            </div>
+            <div>
+                <nav>
+                    <ul>
+                        <li><a href = "dashboard.php">Home</a></li>
+                        <li><a href = "commuting.php">Commuting Insights</a></li>
+                        <li><a href = "energyUse.php">Energy tracker</a></li>
+                        <li><a href = "co2.php">Emissions tracker</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include 'connect.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Core29</title>
+    <link rel="stylesheet" href="public/css/main.css"/>
+    <link rel="icon" type="image" href="public/images/favicon-128x128.png">
+</head>
+<body>
+<div class="loginHero">
+    <div class="loginLogo">
+        <img src="public/images/core29-logo.png" alt="Logo"/>
+    </div>
+</div>
+<div class="login">
+    <div class="loginForm">
+        <form method="post" action="login.php"> <!-- assuming login.php is your login processing script -->
+            <div class="form-group">
+                <input type="text" name="username" class="form-control" placeholder="Username">
+            </div>
+            <div class="form-group">
+                <input type="password" name="password" class="form-control" placeholder="Password">
+            </div>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Login</button>
+            </div>
+        </form>
+    </div>
+</div>
+</body>
+</html>
